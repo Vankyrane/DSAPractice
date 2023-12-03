b79bc1810f3f5a22172f66d839be4fd0c175d1ec
prane <prasaddrane@gmail.com>
2023-12-03 17:59:43 +0000

    committing changes to add DoublyLinkedList methods

---
+++ src/main/java/datastructures/doublylinkedlist/Main.java
@@ -0,0 +1,14 @@
+package datastructures.doublylinkedlist;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DoublyLinkedList myDLL =  new DoublyLinkedList(4);
+
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.printList();
+    }
+}
